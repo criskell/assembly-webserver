a9c2b6297620781eea40e0dc83d2a8a72a4dff09
criskell <cris.moraes.ds@gmail.com>
2024-11-10 06:08:54 -0300

    update

--- tests/foo.asm
+++ tests/foo.asm
@@ -1,6 +1,4 @@
-; nasm -w+all -w+error -w-reloc-abs-dword -f elf64 tests/foo.asm -o out/foo.o && gcc out/foo.o -g -o out/foo -no-pie && ./out/foo criskell
-
-global main
+global _start
 
 %define SYS_write 1
 %define SYS_exit 60
@@ -11,30 +9,31 @@ greet: db "Hi, ", 0
 newline: db 0xA, 0
 
 section .text
-main:
-    push rbp ; definimos rbp como âncora para a nova base da nossa pilha
-    mov rbp, rsp ; criamos um novo stack frame
+_start:
+    push rbp
+    mov rbp, rsp
 
     push greet             ; adiciona "Hi, " na stack para print
     call .print     
-    pop rax
+    pop rax         
 
-    push qword [rsi + 8]
+    push qword [rbp + 24]  ; adiciona ARG1 na stack para print
     call .print
     pop rax
 
     push newline           ; adiciona newline na stack para print
     call .print
     pop rax
-
+    
     pop rbp
-    xor eax, eax
-    ret
+.exit:                     ; label de término do programa
+    mov rdi, 0
+    mov rax, SYS_exit
+    syscall
 .print:                    ; rotina de print no STDOUT
     push rbp
     mov rbp, rsp
-    push rsi
-    
+
     mov rsi, [rbp + 16]     
     mov r9, rsi
     mov rdx, 0
@@ -49,6 +48,5 @@ main:
     mov rax, SYS_write
     syscall
 
-    pop rsi
     pop rbp
     ret
